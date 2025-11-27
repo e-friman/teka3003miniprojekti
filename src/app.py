7883f564d17e10c71d6f0e6213a22ad22ee524cd
Antti <antti.vas@gmail.com>
2025-11-27 13:32:34 +0200

    lomake refactor

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from citation import Citation
 from database import MemoryDatabase
+from lomake import Lomake
 
 citation = Citation("article", "key1", {"author": "author", "title": "title"})
 
@@ -7,5 +8,48 @@ citation = Citation("article", "key1", {"author": "author", "title": "title"})
 db = MemoryDatabase()
 db.add(citation)
 
+lomake = Lomake()
+
+entryt = lomake.entry_tyypit
+
+
+
 print(db.get_all())
 print(str(db.get_all()[0]))
+
+def tervetuloa():
+    #Pylint ei tykkää tuosta globalista, pitää muuttaa myöhemmin tms. -Vilppu
+    #Vaihdoin tämän taas 1 ja 2 koska en jaksa kirjoittaa -Vilppu
+    print(
+        "\n"
+        "Tervetuloa! Anna Komento!\n"
+        "1 = Syötä viite, 2 = Poistu")
+    inp = input("Komento:")
+    return inp
+
+def syota_viite(db):
+    entry = lomake.entry_syote()
+    db_data = lomake.hae_kentat(db, entry)
+    ret = lomake.tayta_lomake(db_data)
+    db.add(ret)
+
+#Ohjelma käynnistyy
+syote = "0"
+
+#Main loop (kts. tkinter myöhemmin?)
+while syote != "2":
+    syote = tervetuloa()
+    if syote == "1":
+        #testidata, tähän joku getData() tms.
+        cit = lomake.tayta_lomake(None)
+        print(str(cit))
+        db.add(cit)
+    elif syote == "3":
+        for c in db.get_all():
+            print(str(c))
+    else:
+        print(
+            "Outo komento!\n")
+
+#Ohjelma sulkeutuu
+print("Ohjelma sulkeutuu!")
--- src/lomake/lomake.py
+++ src/lomake/lomake.py
@@ -1,12 +1,19 @@
+from citation import Citation
 class Lomake:
+    def __init__(self, syote = input):
+        self._input = input
+
     #HUOM! siirsin entryn omasta paketista tänne, koska pylint valittaa.
     #Myös korjattu noita selffejä ym. sen verran mitä se kait haluaa.
     #-Vilppu
     #------------------------------------------------------------------
     #aloitetaan kysymällä entry tyyppiä esim book (@book)
     def entry_syote(self):
-        entry = input("Entry-tyyppi = ")
+        entry = self._input("Entry-tyyppi = ")
         return entry
+
+    def key_syote(self):
+        return self._input("Anna key: ")
     #Dictionary missä on entry tyyppien syötteet
     #esim. book = [author, title, publisher, year]
     #Oikeasti otetaan databasesta ym.
@@ -32,12 +39,18 @@ class Lomake:
     #Kysyy lomakkeen sisällön käyttäjältä ja laittaa sen eteenpäin tallennettavaksi
     def tayta_lomake(self, data):
         #newList = []
-        for field in data:
+        entry = self.entry_syote()
+        key = self.key_syote()
+        kentat = self.hae_kentat(self.entry_tyypit, entry)
+        data = {}
+        for field in kentat:
             #HUOM! Pylint valittaa, että tässä on muka liikaa argumentteja,
             #vaikka se itse haluaa niitä ja koodi ei toimi ilman että tuohonkin
             #laittaa joten enpä tiedä
             #-Vilppu
-            self.kysy_syotetta(field)
+            out = self.kysy_syotetta(field)
+            data[field] = out
             #syote = Lomake.kysySyötettä(obj)
             #newList.append( obj + "=" + syöte)
             #return newList
+        return Citation(entry, key, data)
